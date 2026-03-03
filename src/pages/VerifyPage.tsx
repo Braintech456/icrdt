3c6abc1f76b315b4cfbf4dbfd6563b06add9af5e
Braintech456 <info.braintechcompany@gmail.com>
2026-03-03 15:07:25 +0530

    Updated VerifyPage.tsx

--- src/pages/VerifyPage.tsx
+++ src/pages/VerifyPage.tsx
@@ -1,13 +1,8 @@
 import Verify from '../components/Verify';
-import AffiliationStrip from '../components/AffiliationStrip';
 
 export default function VerifyPage() {
   return (
     <div className="bg-white">
-      {/* Institutional Affiliation Strip */}
-      <AffiliationStrip />
-
-      {/* Main Verification System */}
       <Verify />
     </div>
   );
